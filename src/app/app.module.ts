ce14da20b3bdbc3e0238dac7a936f2579220d5a1
gustavo.caixeta <gustavo.caixeta42@gmail.com>
2022-03-14 22:01:49 -0300

    nova feature principal, header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
+import { MainPageComponent } from './contents/main-page/main-page/main-page.component';
+import { AllProductsComponent } from './contents/all-products/all-products/all-products.component';
+import { LoginComponent } from './contents/login/login/login.component';
+import { ProductDetailsComponent } from './contents/product-details/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainPageComponent,
+    AllProductsComponent,
+    LoginComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
