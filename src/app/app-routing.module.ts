90366beb0be0963f92e6f1d51728854c07f6eceb
Gustavo Caixeta <gustavo.caixeta@sdbusiness.com.br>
2022-03-16 15:25:13 -0300

    responsividade pagina detalhe de produto para tablet e celular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { MainPageComponent } from './contents/main-page/main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './contents/product-details/product-details/product-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
   },
+  {
+    path: 'productDetail',
+    component: ProductDetailsComponent
+  }
 ];
 
 @NgModule({
