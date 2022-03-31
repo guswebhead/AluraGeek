9d707dbf17f79f2db5f7c2a7b5b95ba0666bc87d
gustavo.caixeta <gustavo.caixeta42@gmail.com>
2022-03-30 23:29:10 -0300

    :sparkles: feat: dinamicidade da pagina de details products

--- src/app/contents/product-details/product-details/product-details.component.ts
+++ src/app/contents/product-details/product-details/product-details.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from './../../../services/products/products.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  productId!: number
+  productData: any
+  similarProd: any
+
+  constructor(
+    private prodService: ProductsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((paramMap) => {
+      this.productId = Number(paramMap['idProduct']);
+    })
+
+    this.getProdById()
+  }
+
+  getProdById() {
+    this.prodService.getProducts().subscribe((data) => {
+      let itemFiltered = data.filter((x: any) => x.id == this.productId)
+      this.productData = itemFiltered[0]
+
+
+      this.similarProd = data.filter((x: any) => x.type == this.productData.type)
+
+
+    })
+  }
+
+  goToItem(id: number) {
+    this.router.navigate(['productDetail/' + id])
+    this.ngOnInit()
   }
 
 }
